93c1a2f684593b9f95529586fd35d4793e9bd458
eFGie <filip.grodzki@gmail.com>
2019-08-26 20:55:56 +0200

    first commit

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,45 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/task")
+public class TaskController {
+    @Autowired
+    private DbService service;
+    @Autowired
+    private TaskMapper taskMapper;
+
+    @RequestMapping(method = RequestMethod.GET,value = "getTasks")
+    public List<TaskDto> getTasks() {
+        return taskMapper.mapToTaskDtoList(service.getAllTasks());
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTask")
+    public TaskDto getTask(@RequestParam("ID") Long taskId) {
+        return taskMapper.mapToTaskDto(service.getTaskById(taskId));
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE,value = "deleteTask")
+    public void deleteTask(Long taskId) {
+
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,value = "updateTask")
+    public TaskDto updateTask(TaskDto taskDto) {
+        return new TaskDto(1L,"Edited test title","Test content");
+    }
+
+    @RequestMapping(method = RequestMethod.POST,value = "createTask")
+    public void createTask(TaskDto taskDto) {
+
+    }
+}
+
